7c05ac03abe47fb341934e44fc5b5fdd5117d4ed
Justyna Łuczak <justyna.macniak@gmail.com>
2022-10-21 13:27:07 +0200

    first commit

---
+++ events/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('', views.home, name="home"),
+        path('<int:year>/<str:month>/', views.home, name="home"),
+        path('events/', views.all_events, name="list_events"),
+        path('add_venue/', views.add_venue, name="add_venue"),
+        path('venues/', views.list_venues, name="list_venues"),
+        path('show_venue/<venue_id>', views.show_venue, name="show_venue"),
+        path('search_venues/', views.search_venues, name="search_venues"),
+]
---
+++ events/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
+from django.http import HttpResponseRedirect
+from .models import Event, Venue
+from .forms import VenueForm
+
+
+def search_venues(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        venues = Venue.objects.filter(name__contains=searched)
+        return render(request, 'events/search_venues.html', {'searched': searched, 'venues': venues})
+    else:
+        return render(request, 'events/search_venues.html', {})
+
+
+def show_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    return render(request, 'events/show_venue.html', {'venue': venue})
+
+
+def list_venues(request):
+    venue_list = Venue.objects.all()
+    return render(request, 'events/venue_list.html', {'venue_list': venue_list})
+
+
+def add_venue(request):
+    submitted = False
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue?submitted==True')
+    else:
+        form = VenueForm
+        if 'submitted' in request.GET:
+            submitted = True
+    return render(request, 'events/add_venue.html', {'form': form, 'submitted': submitted})
+
+
+def all_events(request):
+    event_list = Event.objects.all()
+    return render(request, 'events/event_list.html', {'event_list': event_list})
+
+
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    name = "Justyna"
+    month = month.capitalize()
+    #convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    #create calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+    #get datetime
+    now = datetime.now()
+    current_year = now.year
+    #get current time
+    time = now.strftime('%I:%M %p')
+
+    return render(request, 'events/home.html', {
+        "name": name,
+        "year": year,
+        "month": month,
+        "month_number": month_number,
+        "cal": cal,
+        "current_year": current_year,
+        "time": time,
+    })
+
