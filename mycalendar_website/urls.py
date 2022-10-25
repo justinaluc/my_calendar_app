7784bb1e635dfd18085a0b5ed3a3e80b29e7c955
Justyna Łuczak <justyna.macniak@gmail.com>
2022-10-25 14:42:25 +0200

    add new app >members< with further options: to login and logout, with bootstraping

--- mycalendar_website/urls.py
+++ mycalendar_website/urls.py
@@ -4,4 +4,11 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
+
+admin.site.site_header = "My Calendar Admin Page"
+admin.site.site_title = "My Calendar"
+admin.site.index_title = "Welcome to The Admin Area...!"
+
